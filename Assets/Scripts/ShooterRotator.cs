818210fd7e8c0ee7ee8a8c7f2f0ebf1e68574789
seungin <seungin.cha@gmail.com>
2020-01-10 23:23:26 +0900

    Add a gun barrel
    make gun barrel rotate

---
+++ Assets/Scripts/ShooterRotator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterRotator : MonoBehaviour
+{
+	enum RotateState
+	{
+		Idle, Horizontal, Vertical, Ready
+	};
+
+	private RotateState state;
+
+	public float rotateSpeed = 360f;
+
+	private void Update()
+	{
+		switch (state)
+		{
+			case RotateState.Idle:
+				if (Input.GetButtonDown("Fire1"))
+				{
+					state = RotateState.Horizontal;
+				}
+				break;
+
+			case RotateState.Horizontal:
+				if (Input.GetButton("Fire1"))
+				{
+					transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+				}
+				else if (Input.GetButtonUp("Fire1"))
+				{
+					state = RotateState.Vertical;
+				}
+				break;
+
+			case RotateState.Vertical:
+				if (Input.GetButton("Fire1"))
+				{
+					transform.Rotate(new Vector3(rotateSpeed * Time.deltaTime, 0, 0));
+				}
+				else if (Input.GetButtonUp("Fire1"))
+				{
+					state = RotateState.Ready;
+				}
+				break;
+
+			default:
+				break;
+		}
+	}
+}
