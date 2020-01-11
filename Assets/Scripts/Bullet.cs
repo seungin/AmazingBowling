9a7301dde615affd971967c77497179914cf45ab
seungin <seungin.cha@gmail.com>
2020-01-11 10:54:34 +0900

    add a small prop prefab
    Props take damage according to the distance with a bullet. The damage change prop color and take force to it.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+	public LayerMask propLayerMask;
 	public ParticleSystem explosionParticle;
 	public AudioSource explosionAudio;
+
+	public float explosionForce = 1000f;
+	public float explosionDamage = 100f;
+	public float explosionRadius = 20f;
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Plane")
 		{
+			Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius, propLayerMask);
+
+			for (int i = 0; i < colliders.Length; ++i)
+			{
+				Rigidbody body = colliders[i].GetComponent<Rigidbody>();
+				body.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+
+				Prop prop = colliders[i].GetComponent<Prop>();
+				float damage = calculateDamage(prop.transform.position);
+				prop.TakeDamage(damage);
+			}
+
 			explosionParticle.transform.parent = null;
 
 			explosionParticle.Play();
@@ -19,5 +36,13 @@ public class Bullet : MonoBehaviour
 			Destroy(explosionParticle.gameObject, explosionParticle.main.duration);
 			Destroy(gameObject);
 		}
+	}
+
+	private float calculateDamage(Vector3 targetPosition)
+	{
+		float distance = (transform.position - targetPosition).magnitude;
+		float damageRate = (explosionRadius - distance) / explosionRadius;
+		damageRate = Mathf.Max(damageRate, 0);
+		return damageRate * explosionDamage;
 	}
 }
---
+++ Assets/Scripts/Prop.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prop : MonoBehaviour
+{
+	public ParticleSystem explosionPaticle;
+	public float hp = 100f;
+	private float fullHp;
+	private Material material;
+
+	private void Start()
+	{
+		material = GetComponent<MeshRenderer>().material;
+		fullHp = hp;
+	}
+
+	public void TakeDamage(float damage)
+	{
+		hp -= damage;
+		Debug.Log("HP: " + hp);
+
+		applyDamageColor();
+
+		if (hp <= 0)
+		{
+			ParticleSystem instance = Instantiate(explosionPaticle);
+			AudioSource audio = instance.GetComponent<AudioSource>();
+
+			instance.Play();
+			audio.Play();
+
+			Destroy(instance.gameObject, instance.main.duration);
+
+			gameObject.SetActive(false);
+		}
+	}
+
+	private void applyDamageColor()
+	{
+		Color hpColor = Color.green;
+		float hpRate = hp / fullHp;
+
+		if (hpRate > 0.5f)
+		{
+			hpColor.r = -2f * hpRate + 2f;
+			hpColor.g = 1f;
+			hpColor.b = 0f;
+		}
+		else
+		{
+			hpColor.r = 1f;
+			hpColor.g = 2f * hpRate;
+			hpColor.b = 0f;
+		}
+
+		material.color = hpColor;
+	}
+}
