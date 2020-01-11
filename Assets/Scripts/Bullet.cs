4d4f35872f5cf98c1e25cfd9707c1b67b1e2bffb
seungin <seungin.cha@gmail.com>
2020-01-11 09:12:42 +0900

    Add a bullet prefab
    bullet has particle and audio effect for explosion

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	public ParticleSystem explosionParticle;
+	public AudioSource explosionAudio;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Plane")
+		{
+			explosionParticle.transform.parent = null;
+
+			explosionParticle.Play();
+			explosionAudio.Play();
+
+			Destroy(explosionParticle.gameObject, explosionParticle.main.duration);
+			Destroy(gameObject);
+		}
+	}
+}
