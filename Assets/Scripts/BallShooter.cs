91dc63d58f2e2b3ce064a6cb0878db61b7123a1a
seungin <seungin.cha@gmail.com>
2020-01-12 21:23:49 +0900

    add sound effects for shooter
    apply charging and fire sound effects

--- Assets/Scripts/BallShooter.cs
+++ Assets/Scripts/BallShooter.cs
@@ -8,6 +8,9 @@ public class BallShooter : MonoBehaviour
 	public Rigidbody bullet;
 	public Transform gunpoint;
 	public Slider powerGauge;
+	public AudioSource shootingAudio;
+	public AudioClip chargingClip;
+	public AudioClip fireClip;
 
 	public float minForce = 15f;
 	public float maxForce = 30f;
@@ -21,10 +24,15 @@ public class BallShooter : MonoBehaviour
 		if (Input.GetButtonDown("Fire1"))
 		{
 			targetForce = minForce;
+			powerGauge.value = minForce;
+
+			shootingAudio.clip = chargingClip;
+			shootingAudio.Play();
 		}
 		else if (Input.GetButton("Fire1"))
 		{
 			chargingSpeed = (maxForce - minForce) / chargingTime;
+
 			targetForce += chargingSpeed * Time.deltaTime;
 			powerGauge.value = targetForce;
 		}
@@ -32,7 +40,12 @@ public class BallShooter : MonoBehaviour
 		{
 			Rigidbody instance = Instantiate(bullet, gunpoint.position, gunpoint.rotation);
 			instance.velocity = targetForce * gunpoint.forward;
+
+			targetForce = minForce;
 			powerGauge.value = minForce;
+
+			shootingAudio.clip = fireClip;
+			shootingAudio.Play();
 		}
 	}
 }
