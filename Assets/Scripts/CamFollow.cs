76a08b8928e2d4f66721464bfcb999dcfbe94bff
seungin <seungin.cha@gmail.com>
2020-01-13 06:18:33 +0900

    add a cam follow
    camera rig follows a target with smooth damp

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+	public enum State
+	{
+		Idle,
+		Ready,
+		Tracking
+	}
+
+	private State state
+	{
+		set
+		{
+			switch (value)
+			{
+				case State.Idle:
+					targetZoomSize = roundReadyZoomSize;
+					break;
+
+				case State.Ready:
+					targetZoomSize = readyShotZoomSize;
+					break;
+
+				case State.Tracking:
+					targetZoomSize = trackingZoomSize;
+					break;
+			}
+		}
+	}
+
+	public float smoothTime = 0.2f;
+
+	public Transform target;
+	private Vector3 targetPosition;
+	private Vector3 lastMovingVelocity;
+
+	private Camera cam;
+	private float lastZoomSpeed;
+	private float targetZoomSize = 5f;
+	private const float roundReadyZoomSize = 14.5f;
+	private const float readyShotZoomSize = 5f;
+	private const float trackingZoomSize = 10f;
+
+
+	private void Awake()
+	{
+		cam = GetComponentInChildren<Camera>();
+		state = State.Idle;
+	}
+
+	private void FixedUpdate()
+	{
+		if (target != null)
+		{
+			Move();
+			Zoom();
+		}
+	}
+
+	public void SetTarget(Transform newTarget, State newState)
+	{
+		target = newTarget;
+		state = newState;
+	}
+
+	private void Reset()
+	{
+		state = State.Idle;
+	}
+
+	private void Move()
+	{
+		targetPosition = target.transform.position;
+		Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref lastMovingVelocity, smoothTime);
+		transform.position = smoothPosition;
+	}
+
+	private void Zoom()
+	{
+		float smoothZoomSize = Mathf.SmoothDamp(cam.orthographicSize, targetZoomSize, ref lastZoomSpeed, smoothTime);
+		cam.orthographicSize = smoothZoomSize;
+	}
+}
